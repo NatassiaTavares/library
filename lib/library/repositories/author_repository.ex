09499c90dea22ff1f3ca02aee6ccfffd45062825
Natassia Tavares <natassiaxtavares@gmail.com>
2021-06-06 18:08:01 -0300

    remove book files, added crud in repository

--- lib/library/repositories/author_repository.ex
+++ lib/library/repositories/author_repository.ex
@@ -4,6 +4,26 @@ defmodule Library.Repositories.Author do
 
   import Ecto.Query
 
+  def create(attrs) do
+    %Author{}
+    |> Author.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update(%Author{} = author, attrs) do
+    author
+    |> Author.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete(%Author{} = author) do
+    Repo.delete(author)
+  end
+
+  def get_all() do
+    Repo.all(Author)
+  end
+
   def get_names() do
     query = from(a in Author,
      select: a.name)
